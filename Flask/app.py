2b1688780432723411bf70819fa1060858752d54
estebanaragonc <55291655+estebanaragonc@users.noreply.github.com>
2023-10-10 16:56:24 -0500

    agregando mas columnas

--- Flask/app.py
+++ Flask/app.py
@@ -22,8 +22,53 @@ def fetch_betmgm(ttl_hash=None):
     del ttl_hash
     return fetch_game_data(sportsbook="betmgm")
 
+@lru_cache()
+def fetch_fanduel_xgb(ttl_hash=None):
+    del ttl_hash
+    return fetch_game_data_xgb(sportsbook="fanduel_xgb")
+
+@lru_cache()
+def fetch_draftkings_xgb(ttl_hash=None):
+    del ttl_hash
+    return fetch_game_data_xgb(sportsbook="draftkings_xgb")
+
+@lru_cache()
+def fetch_betmgm_xgb(ttl_hash=None):
+    del ttl_hash
+    return fetch_game_data(sportsbook="betmgm_xgb")
+
 def fetch_game_data(sportsbook="fanduel"):
     cmd = ["python", "main.py", "-nn", f"-odds={sportsbook}"]
+    stdout = subprocess.check_output(cmd, cwd="../").decode()
+    data_re = re.compile(r'\n(?P<home_team>[\w ]+)(\((?P<home_confidence>[\d+\.]+)%\))? vs (?P<away_team>[\w ]+)(\((?P<away_confidence>[\d+\.]+)%\))?: (?P<ou_pick>OVER|UNDER) (?P<ou_value>[\d+\.]+) (\((?P<ou_confidence>[\d+\.]+)%\))?', re.MULTILINE)
+    ev_re = re.compile(r'(?P<team>[\w ]+) EV: (?P<ev>[-\d+\.]+)', re.MULTILINE)
+    odds_re = re.compile(r'(?P<away_team>[\w ]+) \((?P<away_team_odds>-?\d+)\) @ (?P<home_team>[\w ]+) \((?P<home_team_odds>-?\d+)\)', re.MULTILINE)
+    games = {}
+    for match in data_re.finditer(stdout):
+        game_dict = {'away_team': match.group('away_team').strip(),
+                     'home_team': match.group('home_team').strip(),
+                     'away_confidence': match.group('away_confidence'),
+                     'home_confidence': match.group('home_confidence'),
+                     'ou_pick': match.group('ou_pick'),
+                     'ou_value': match.group('ou_value'),
+                     'ou_confidence': match.group('ou_confidence')}
+        for ev_match in ev_re.finditer(stdout):
+            if ev_match.group('team') == game_dict['away_team']:
+                game_dict['away_team_ev'] = ev_match.group('ev')
+            if ev_match.group('team') == game_dict['home_team']:
+                game_dict['home_team_ev'] = ev_match.group('ev')
+        for odds_match in odds_re.finditer(stdout):
+            if odds_match.group('away_team') == game_dict['away_team']:
+                game_dict['away_team_odds'] = odds_match.group('away_team_odds')
+            if odds_match.group('home_team') == game_dict['home_team']:
+                game_dict['home_team_odds'] = odds_match.group('home_team_odds')
+
+        print(json.dumps(game_dict, sort_keys=True, indent=4))
+        games[f"{game_dict['away_team']}:{game_dict['home_team']}"] = game_dict
+    return games
+
+def fetch_game_data_xgb(sportsbook="fanduel"):
+    cmd = ["python", "main.py", "-xgb", f"-odds={sportsbook}"]
     stdout = subprocess.check_output(cmd, cwd="../").decode()
     data_re = re.compile(r'\n(?P<home_team>[\w ]+)(\((?P<home_confidence>[\d+\.]+)%\))? vs (?P<away_team>[\w ]+)(\((?P<away_confidence>[\d+\.]+)%\))?: (?P<ou_pick>OVER|UNDER) (?P<ou_value>[\d+\.]+) (\((?P<ou_confidence>[\d+\.]+)%\))?', re.MULTILINE)
     ev_re = re.compile(r'(?P<team>[\w ]+) EV: (?P<ev>[-\d+\.]+)', re.MULTILINE)
@@ -68,4 +113,8 @@ def index():
     draftkings = fetch_draftkings(ttl_hash=get_ttl_hash())
     betmgm = fetch_betmgm(ttl_hash=get_ttl_hash())
 
-    return render_template('index.html', today=date.today(), data={"fanduel": fanduel, "draftkings": draftkings, "betmgm": betmgm})
+    fanduel_xgb = fetch_fanduel_xgb(ttl_hash=get_ttl_hash())
+    draftkings_xgb = fetch_draftkings_xgb(ttl_hash=get_ttl_hash())
+    betmgm_xgb = fetch_betmgm_xgb(ttl_hash=get_ttl_hash())
+
+    return render_template('index.html', today=date.today(), data={"fanduel": fanduel, "draftkings": draftkings, "betmgm": betmgm, "fanduel_xgb": fanduel_xgb, "draftkings_xgb": draftkings_xgb, "betmgm_xgb": betmgm_xgb})
